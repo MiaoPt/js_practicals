a539b89aee6fec48e8e2b103300a74b73712241b
MiaoPt <mickyzhang0817@gmail.com>
2026-01-21 18:52:56 +0000

    Task 2: Counter Logic

--- practiclas.js
+++ practiclas.js
@@ -8,3 +8,24 @@ btn.addEventListener('click', () => {
     btn.classList.replace('btn-success', 'btn-primary');
     btn.disabled = true;
 })
+
+incrementBtn = document.getElementById('increment-btn')
+
+
+incrementBtn.addEventListener('click', () => {
+    num = Number(document.getElementById('counter-value').innerHTML);
+    num = num + 1;
+    document.getElementById('counter-value').innerHTML = `${num}`
+})
+
+decrementBtn = document.getElementById('decrement-btn')
+decrementBtn.addEventListener('click', () => {
+    num = Number(document.getElementById('counter-value').innerHTML);
+    if (num == 0) {
+        btn.disabled = true;
+    } else {
+        num = num - 1;
+        document.getElementById('counter-value').innerHTML = `${num}`
+    }
+
+})
