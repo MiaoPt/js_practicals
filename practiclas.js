8c773a842671d9e6e5fc33f8316aa6798d938c93
MiaoPt <mickyzhang0817@gmail.com>
2026-01-22 20:18:04 +0000

    Task 6: Custom Form Validation

--- practiclas.js
+++ practiclas.js
@@ -1,24 +1,22 @@
 
-
-btn = document.getElementById('primary-action-btn')
-
+//Task 1
+const btn = document.getElementById('primary-action-btn')
 btn.addEventListener('click', () => {
     btn.innerHTML = 'Clicked!';
     btn.classList.replace('btn-primary', 'btn-success') ||
     btn.classList.replace('btn-success', 'btn-primary');
-    btn.disabled = true;
+    btn.disabled = true; //*** */
 })
 
-incrementBtn = document.getElementById('increment-btn')
-
-
+//Task 2  improve the repeated code
+const incrementBtn = document.getElementById('increment-btn')
 incrementBtn.addEventListener('click', () => {
     num = Number(document.getElementById('counter-value').innerHTML);
     num = num + 1;
-    document.getElementById('counter-value').innerHTML = `${num}`
+    document.getElementById('counter-value').innerHTML = `${num}` //***
 })
 
-decrementBtn = document.getElementById('decrement-btn')
+const decrementBtn = document.getElementById('decrement-btn')
 decrementBtn.addEventListener('click', () => {
     num = Number(document.getElementById('counter-value').innerHTML);
     if (num == 0) {
@@ -30,15 +28,18 @@ decrementBtn.addEventListener('click', () => {
 
 })
 
-alertBox = document.getElementById('alert-box')
-alertBtn = document.getElementById('toggle-alert-btn')
+
+//Task 3
+const alertBox = document.getElementById('alert-box')
+const alertBtn = document.getElementById('toggle-alert-btn')
 
 alertBtn.addEventListener('click', () => {
     alertBox.classList.toggle('d-none');
 
 })
 
-viewBtns = document.querySelectorAll('button[data-view]')
+// Task 4 & 5
+const viewBtns = document.querySelectorAll('button[data-view]')
 viewBtns.forEach(viewBtn => {
     viewBtn.addEventListener('click', () => {
             viewBtns.forEach(viewBtn => viewBtn.classList.remove('active'));
@@ -47,3 +48,22 @@ viewBtns.forEach(viewBtn => {
         
     }) 
 });
+
+
+// Task 6
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    nameInput = document.getElementById('name').value  //*** 
+    emailInput = document.getElementById('email').value
+    messageInput = document.getElementById('message').value
+    if (! nameInput) {
+        document.getElementById('error-message').innerHTML = 'Please put your name!'
+    } else if (! emailInput) {
+        document.getElementById('error-message').innerHTML = 'Please put your email!'
+    } else if (! messageInput) {
+        document.getElementById('error-message').innerHTML = 'Please put your message!'
+    } else {
+        return
+    }
+})
